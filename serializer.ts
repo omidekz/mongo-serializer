a8f50f61ee1f51058e531523aa16b5ba5876cde3
omidekz <omidekz@gmail.com>
2020-10-12 14:44:03 +0330

    fix ts

--- serializer.ts
+++ serializer.ts
@@ -11,29 +11,13 @@ class Serializer{
             all_fields: Array<string>
             select_fields: string[]
             result: object | object[]
-            constructor(model_instance, many: Boolean = false){
+            many: boolean = false
+            model_instance: object | object[]
+            constructor(model_instance, many: boolean = false){
                 this.all_fields = Object.keys(Model.schema.obj)
-                for (let i = 0 ; i < fields.length ; i++){
-                    if (
-                        !this.all_fields.indexOf(fields[i]) &&
-                        typeof(this[fields[i]]) != 'function'
-                    ){
-                        throw new Error(fields[i]+" object | class have not this field | method")
-                    }
-                }
+                this.model_instance = model_instance
+                this.many = many
                 this.select_fields = fields
-                this.init(async () =>{
-                    if (many){
-                        this.result = []
-                        if (Array.isArray(this.result))
-                        for await (const doc of model_instance){
-                            this.result.push(this.handle_document(doc))
-                        }
-                    }else{
-                        this.result = this.handle_document(model_instance)
-                    }
-                    return model_instance
-                })
             }
             private async handle_document(pure_doc){
                 let result_object: object = {
@@ -41,9 +25,9 @@ class Serializer{
                 }
                 let select_fields = this.select_fields
                 for (let i = 0 ; i < select_fields.length ; i++){
-                    if (typeof(this[select_fields[i]]) == "function"){
+                    if (typeof(this[select_fields[i]+"_field"]) == "function"){
                         result_object[select_fields[i]] = await (
-                            this[select_fields[i]].bind(this)(pure_doc)
+                            this[select_fields[i]] = this[select_fields[i]+"_field"].bind(this)(pure_doc)
                         )
                     }else{
                         result_object[select_fields[i]] = pure_doc[
@@ -53,8 +37,29 @@ class Serializer{
                 }
                 return result_object
             }
-            init(callback){
-                callback.bind(this)()
+            async to_json(){
+                this.model_instance = await this.model_instance
+                fields = this.select_fields
+                
+                for (let i = 0 ; i < fields.length ; i++){
+                    if (
+                        !this.all_fields.indexOf(fields[i]) &&
+                        typeof(this[fields[i]+"_field"]) != 'function'
+                    ){
+                        throw new Error(fields[i]+" object | class have not this field | method")
+                    }
+                }
+                if (this.many){
+                    this.result = []
+                }
+                if (this.many && Array.isArray(this.result) && Array.isArray(this.model_instance)){
+                        for (const doc of this.model_instance){
+                            this.result.push(await this.handle_document(doc))
+                        }
+                    }else{
+                        this.result = this.handle_document(await this.model_instance)
+                    }
+                return this.result
             }
         }
         return dynamic
